a5a473fab923991c56d3cfbcde43677e1d741be6
BrandinO771 <brandon.steinke@yahoo.com>
2019-08-12 17:04:18 -0700

    this is a small update to para py

--- Bonus HW/hw3_pypara_bs.py
+++ Bonus HW/hw3_pypara_bs.py
@@ -1,48 +1,53 @@
 '''
-PyParagraph
-![Language](Images/language.png)
+PYTHON BONUS HW  | PyParagraph  | BRANDON STEINK  |  8-12-19
+========================================================================'''
+''' SUMMARY INSTRUCTIONS 
+    ![Language](Images/language.png)
 
-In this challenge, you get to play the role of chief linguist at a local learning academy. As chief linguist, you are responsible for assessing the complexity of various passages of writing, ranging from the sophomoric Twilight novel to the nauseatingly high-minded research article. Having read so many passages, you've since come up with a fairly simple set of metrics for assessing complexity.
+    In this challenge, you get to play the role of chief linguist at a local learning academy. As chief linguist, you are responsible for assessing the complexity of various passages of writing, ranging from the sophomoric Twilight novel to the nauseatingly high-minded research article. Having read so many passages, you've since come up with a fairly simple set of metrics for assessing complexity.
 
-Your task is to create a Python script to automate the analysis of any such passage using these metrics. Your script will need to do the following:
-* Import a text file filled with a paragraph of your choosing.
-* Assess the passage for each of the following:
-  * Approximate word count
-  * Approximate sentence count
-  * Approximate letter count (per word)
-  * Average sentence length (in words)
-* As an example, this passage:
+    Your task is to create a Python script to automate the analysis of any such passage using these metrics. Your script will need to do the following:
+    * Import a text file filled with a paragraph of your choosing.
+    * Assess the passage for each of the following:
+    * Approximate word count
+    * Approximate sentence count
+    * Approximate letter count (per word)
+    * Average sentence length (in words)
+    * As an example, this passage:
 
-> “Adam Wayne, the conqueror, with his face flung back and his mane like a lion's, 
-stood with his great sword point upwards, the red raiment of his office flapping 
-around him like the red wings of an archangel. And the King saw, he knew not how, 
-something new and overwhelming. The great green trees and the great red robes swung 
-together in the wind. The preposterous masquerade, born of his own mockery, towered 
-over him and embraced the world. This was the normal, this was sanity, this was nature, 
-and he himself, with his rationality, and his detachment and his black frock-coat, he was 
-the exception and the accident a blot of black upon a world of crimson and gold.”
-...would yield these results:
-```output
-Paragraph Analysis
------------------
-Approximate Word Count: 122
-Approximate Sentence Count: 5
-Average Letter Count: 4.6
-Average Sentence Length: 24.0
-```
-* **Special Hint:** You may find this code snippet helpful when determining sentence length (look into [regular expressions](https://en.wikipedia.org/wiki/Regular_expression) if interested in learning more):
-```python
-import re
-re.split("(?<=[.!?]) +", paragraph)
-```
+    > “Adam Wayne, the conqueror, with his face flung back and his mane like a lion's, 
+    stood with his great sword point upwards, the red raiment of his office flapping 
+    around him like the red wings of an archangel. And the King saw, he knew not how, 
+    something new and overwhelming. The great green trees and the great red robes swung 
+    together in the wind. The preposterous masquerade, born of his own mockery, towered 
+    over him and embraced the world. This was the normal, this was sanity, this was nature, 
+    and he himself, with his rationality, and his detachment and his black frock-coat, he was 
+    the exception and the accident a blot of black upon a world of crimson and gold.”
+    ...would yield these results:
+    ```output
+    Paragraph Analysis
+    -----------------
+    Approximate Word Count: 122
+    Approximate Sentence Count: 5
+    Average Letter Count: 4.6
+    Average Sentence Length: 24.0
+    ```
+    * **Special Hint:** You may find this code snippet helpful when determining sentence length (look into [regular expressions](https://en.wikipedia.org/wiki/Regular_expression) if interested in learning more):
+    ```python
+    import re
+    re.split("(?<=[.!?]) +", paragraph)
+    =================================================================================================
 '''
+
+
 import os 
 import re
 import csv
-   # wordToScramble = re.split(r'\W*',initialWord) 
-    # this breaks the word into individual letters each indexed so can be shuffled    
-    #print(initialWord) # this shows how the word is split into letters 
-# THIS IS MY CUSTOM AVG FUNC
+
+
+''' THIS IS MY CUSTOM AVG FUNC
+========================================================================'''
+
 def average_func(the_input_List):
     
     total = sum(the_input_List)
@@ -50,35 +55,37 @@ def average_func(the_input_List):
     return total / number_of_elements
 
 
-file = "resources/paragraph_1.txt"
-#file = "./resources/paragraph_1.txt"
+''' IMPORT FILE 
+========================================================================'''
 
-# Open the file in "read" mode ('r') and store the contents in the variable "text"
-with open(file, 'r') as text:
-    # This stores a reference to a file stream
-    #print(text)
-    # Store all of the text inside a variable called "lines"
-    lines = text.read()
-    # Print the contents of the text file
-    #print(lines)
+file = "resources/paragraph_1.txt"  #file = "./resources/paragraph_1.txt"
+
+with open(file, 'r') as text:  #Open the file in "read" mode ('r') and store the contents in the variable "text"
+   
+    lines = text.read()  # This stores a reference to a file stream # Store all of the text inside a variable called "lines"
+
+
+
+''' CALCS AND SPLITS 
+========================================================================'''
+    
 all_char_list =[lines]
 indv_words = re.split(" ", lines) # SPLIT  LIST AT EACH SPACES 
 indv_sent = re.split("(?<=[.!?]) +", lines) # SPLIT AT EACH FOLLOWING PUNC
-#print("the indvid words are")
-#print(indv_words)
-
 word_count = len(indv_words)
 sent_count = len(indv_sent)
+#print("the indvid words are")
+#print(indv_words)
 #print(f"Total Words : {word_count}")
 #print(f"Total Sentences : {sent_count} ")
 
+
+
+''' ITERATE SENTENCE LIST BREAK N2 WORDS AND COUNT  
+========================================================================'''
+
 total_sent1 = 0 
-total_sent2 = 0 
-total_sent3 = 0 
-total_sent4 = 0 
-total_sent5 = 0 
 totalAvgLtrsList = []
-# iterate through sentence list, and break each sent n2 words and count 
 words = -1
 for w in indv_sent : 
 
@@ -95,6 +102,11 @@ for elements in all_char_list :
 total_avg_letters =  (char_count - word_count - sent_count) / word_count  
 avg_letters_formt = "{:.2}".format(total_avg_letters) 
 
+
+
+''' TERMINAL PRINT OUT 
+========================================================================'''
+
 print("Paragraph Analysis")
 print("-----------------")
 print(f"Approx Word Count: {word_count}")
@@ -105,14 +117,13 @@ print(f"The Total characters with spaces are {char_count}")
 print("The Total characters without spaces are " +  str(char_count - word_count - 1))
 
 
+''' FINAL LISTS AND CSV OUTPUT
+========================================================================'''
+
 listA = ["Approx Word Count:","Approx Sentence Count:", "The Average Letter Per Word Count:", "The Average Sent Length is:"   ]
 listB = [ word_count, sent_count, avg_letters_formt, avg_sent_length]
-
 data_table_to_insert = zip(listA, listB )  
 
-# // write like this if want  to put in specific folder
-# new_output_file = os.path.join("", "resources", "new_para_eval.csv")
-# // write like this if want in curr folder as this file 
 new_output_file = os.path.join("", "new_para_eval.csv")
 with open(new_output_file, "w", newline="") as csvfile:
     data_to_be_inserted = csv.writer(csvfile, delimiter=',')
@@ -124,7 +135,13 @@ print("your csv is now complete")
 
 
 '''
+END OF ACTIVE CODE 
 ===================================================================================
+
+# // write like this if want  to put in specific folder
+# new_output_file = os.path.join("", "resources", "new_para_eval.csv")
+# // write like this if want in curr folder as this file 
+
 # below is an over engineered turd replaced by above
 #words +=1    
 #if words <= sent_count :
